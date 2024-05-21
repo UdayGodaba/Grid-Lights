79d687a7370aa671d141b88f28a85aeec8b3688e
UdayGodaba <godabauday@gmail.com>
2024-05-21 21:18:42 +0530

    UI is configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import GridLights from "./components/GridLights";
 
 function App() {
-  return <></>;
+  const gridConfig = [
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ];
+
+  return (
+    <div className="app" style={{ "--length": gridConfig[0].length }}>
+      <div className="heading">Grid Lights</div>
+      <GridLights grid={gridConfig} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/GridLights.jsx
+++ src/components/GridLights.jsx
@@ -1,5 +1,14 @@
-const GridLights = () => {
-  return <div>GridLights</div>;
+/* eslint-disable react/prop-types */
+const GridLights = ({ grid }) => {
+  return (
+    <div className="grid">
+      {grid
+        .flat(1)
+        .map((val, idx) =>
+          val ? <button key={idx}></button> : <span key={idx}> </span>
+        )}
+    </div>
+  );
 };
 
 export default GridLights;
